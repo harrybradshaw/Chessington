05ca1960b3a59c955f0a5968a087a86f75982b09
harrybradshaw <harrylewisbradshaw@googlemail.com>
2021-11-23 11:42:48 +0000

    Check pawns can move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,6 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
+            var curSquare = board.FindPiece(this);
+            var moves = new List<Square>();
+            if (this.Player == Player.White)
+            {
+                if (curSquare.Row > 1 && board.GetPiece(new Square(curSquare.Row - 1, curSquare.Col)) == null)
+                {
+                    moves.Add(new Square(curSquare.Row - 1, curSquare.Col));
+                    return moves;
+                }
+            }
+            else if (this.Player == Player.Black)
+            {
+                if (curSquare.Row < 8 && board.GetPiece(new Square(curSquare.Row + 1, curSquare.Col)) == null)
+                {
+                    moves.Add(new Square(curSquare.Row + 1, curSquare.Col));
+                    return moves;
+                }
+            }
             return Enumerable.Empty<Square>();
         }
     }
