2a8df498728f2d53b8330f6afd3a37d5550f6430
harrybradshaw <harrylewisbradshaw@googlemail.com>
2021-11-23 12:29:20 +0000

    Queens can move, not to the same square as already at!

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,49 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availMoves = new List<Square>();
+            var curSquare = board.FindPiece(this);
+            
+            for (int i = 0; i < 8; i++)
+            {
+                var newSquareX = new Square(curSquare.Row,i);
+                if (!newSquareX.Equals(curSquare))
+                {
+                    availMoves.Add(newSquareX);
+                }
+                var newSquareY = new Square(i,curSquare.Col);
+                if (!newSquareY.Equals(curSquare))
+                {
+                    availMoves.Add(newSquareY);
+                }
+               
+            }
+            var newSquare = new Square(curSquare.Row + 1, curSquare.Col + 1);
+            while (board.IsValid(newSquare))
+            {
+                availMoves.Add(newSquare);
+                newSquare = new Square(newSquare.Row + 1, newSquare.Col + 1);
+            }
+            newSquare = new Square(curSquare.Row + 1, curSquare.Col -1 );
+            while (board.IsValid(newSquare))
+            {
+                availMoves.Add(newSquare);
+                newSquare = new Square(newSquare.Row + 1, newSquare.Col - 1);
+            }
+            newSquare = new Square(curSquare.Row - 1, curSquare.Col + 1);
+            while (board.IsValid(newSquare))
+            {
+                availMoves.Add(newSquare);
+                newSquare = new Square(newSquare.Row - 1, newSquare.Col + 1);
+            }
+            newSquare = new Square(curSquare.Row - 1, curSquare.Col - 1);
+            while (board.IsValid(newSquare))
+            {
+                availMoves.Add(newSquare);
+                newSquare = new Square(newSquare.Row - 1, newSquare.Col - 1);
+            }
+            
+            return availMoves;
         }
     }
 }
